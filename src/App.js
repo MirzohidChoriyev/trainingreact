423915a8a0bd09f90cfc0ec047a07d532f9c7a1d
MirzohidChoriyev <90138286+MirzohidChoriyev@users.noreply.github.com>
2021-10-27 00:42:18 +0500

    Choriyev Mirzohid
    O'zgartirildi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./App.css"
+import Import from './Data/Import'
 
 export default function App(){
     return(
         <div>
             Choriyev Mirzohid...
+            <Import />  
         </div>
     )
 }
---
+++ src/Data/data.js
@@ -0,0 +1,26 @@
+[
+{
+	id: 1,
+	name: 'Mirzohid',
+	age: '20',
+	lastname: 'Choriyev'
+},
+{
+	id: 2,
+	name: 'Mirzohid',
+	age: '20',
+	lastname: 'Choriyev'
+},
+{
+	id: 3,
+	name: 'Mirzohid',
+	age: '20',
+	lastname: 'Choriyev'
+},
+{
+	id: 4,
+	name: 'Mirzohid',
+	age: '20',
+	lastname: 'Choriyev'
+}
+]
